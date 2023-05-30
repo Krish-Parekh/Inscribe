38e6c54d058de4b305544d3096e5b4b66135a0af
Krish-Parekh <krishparekh303@gmail.com>
2023-05-30 19:14:09 +0530

    Inscribe: Implemented NewNotePage component.

--- client-web/src/pages/UserNotesPage/NewNotePage.jsx
+++ client-web/src/pages/UserNotesPage/NewNotePage.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { RiDeleteBin5Line, RiCheckLine } from "react-icons/ri";
+import "./NewNotePage.css";
 
 const NewNotePage = () => {
-  return <div>NewNotePage</div>;
+  return (
+    <div className="new-notes__wrapper">
+      <div className="input__container">
+        <input type="text" placeholder="Title" className="input__title"/>
+        <div className="icon__container">
+          <RiDeleteBin5Line className="input__icon"/>
+          <RiCheckLine className="input__icon"/>
+        </div>
+      </div>
+      <textarea placeholder="Start writing here!!!" className="input__content"/>
+    </div>
+  );
 };
 
 export default NewNotePage;
