227ceb8950fde72f574e28a0bf177b27fbf3d242
Krish-Parekh <krishparekh303@gmail.com>
2023-05-31 13:18:12 +0530

    Inscribe: Implemented custom-button color pallet.

--- client-web/src/pages/UserNotesPage/NewNotePage.jsx
+++ client-web/src/pages/UserNotesPage/NewNotePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { RiDeleteBin5Line, RiCheckLine } from "react-icons/ri";
 import "./NewNotePage.css";
 
@@ -13,6 +13,12 @@ const NewNotePage = () => {
         </div>
       </div>
       <textarea placeholder="Start writing here!!!" className="input__content"/>
+      <div className="custom__bottom">
+        <div className="color__one color__button"></div>
+        <div className="color__two color__button"></div>
+        <div className="color__three color__button"></div>
+        <div className="color__four color__button"></div>
+      </div>
     </div>
   );
 };
