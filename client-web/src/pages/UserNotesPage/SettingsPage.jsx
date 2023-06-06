73aa7c395eca3ef8e99f90d7aee57b7d59dd6507
Krish-Parekh <krishparekh303@gmail.com>
2023-06-06 10:53:14 +0530

    Inscribe: Bug fixes in SettingsPage.

--- client-web/src/pages/UserNotesPage/SettingsPage.jsx
+++ client-web/src/pages/UserNotesPage/SettingsPage.jsx
@@ -1,38 +1,43 @@
 import React from "react";
-import { CiLight } from "react-icons/ci";
+import { BsFillSunFill } from "react-icons/bs";
 import { FaRegMoon } from "react-icons/fa";
 import "./SettingsPage.css";
 const SettingsPage = () => {
   return (
-    <div className="setting__wrapper">
-      {/* <h1>⚙️ Settings</h1> */}
-      <section className="first">
-        <h2>Personal Information</h2>
-        <div className="personal__info">
-          <div className="personal__info__item">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" />
+    <div className="settings__wrapper">
+      <h1>⚙️ Settings</h1>
+      <div className="settings__container">
+        <div className="first">
+          <h2>Personal Information</h2>
+          <div className="personal__info">
+            <div className="personal__info__item">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" placeholder="username" />
+            </div>
+            <div className="personal__info__item">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="joe@gmail.com"
+              />
+            </div>
           </div>
-          <div className="personal__info__item">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+          <button className="btn btn__primary">Save</button>
+        </div>
+        <div className="second">
+          <h2>Appearance</h2>
+          <div className="appearance__item">
+            <BsFillSunFill className="appearance__icon"/>
+            <p>Light Mode</p>
+          </div>
+          <div className="appearance__item">
+            <FaRegMoon className="appearance__icon"/>
+            <p>Dark Mode</p>
           </div>
         </div>
-
-        <button className="btn btn--primary">Save</button>
-      </section>
-      <section className="second">
-        <h2>Appearance</h2>
-        <div className="appearance__info">
-          <div className="appearance__info__item">
-            <label htmlFor="theme">Theme</label>
-            <select name="theme" id="theme">
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-            </select>
-          </div>
-        </div>
-      </section>
+      </div>
     </div>
   );
 };
